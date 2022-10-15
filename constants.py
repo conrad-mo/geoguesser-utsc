1f8065f83d909e230238488f3ecf130e38775d3f
Nitanon Rachapradit <98104447+Blue-Peanuts@users.noreply.github.com>
2022-10-14 22:36:37 -0400

    Added constants, added database template, and a bit of game functions body

---
+++ constants.py
@@ -0,0 +1,4 @@
+MAX_ROUND = 5
+STATE_MAIN = 'Main State'
+STATE_GUESS = 'Guess State'
+STATE_END = 'End State'
---
+++ game_functions.py
@@ -0,0 +1,18 @@
+from typing import List
+
+def calculate_score(x: int, y: int, photo_id: int) -> int:
+    """
+    """
+    pass
+
+
+def get_photo_coords(photo_id: int) -> int:
+    """
+    """
+    pass
+
+
+def get_random_picture(lst: List[int]) -> int:
+    """
+    """
+    pass
