2353cb12cd198d841a7700e57c403487a5d31554
Shaul-Le <shaulwle@gmail.com>
2022-10-15 10:50:32 -0400

    Completed functions countdown, guess, and next_round

--- frame_game.py
+++ frame_game.py
@@ -3,6 +3,7 @@ from tkinter import *
 from tkinter import ttk
 import game_functions as gf
 
+unused_photos = [0,1,2]
 
 time_left = 30
 current_photo_id = 0
@@ -24,24 +25,44 @@ timer.place(relx=0.5, rely=0.03, anchor='center')
 
 #countdown function
 def countdown():
+    """Decrease time_left by 1. Then, call function lose() if time_left is 0. 
+    Else, update timer text and call countdown after 1 second.
+    
+    """
     global time_left
     time_left = time_left - 1
     
-    #if the timer hits 0, call lose()
+    if time_left == 0:
+        lose()
+    else:
+        timer['text'] = time_left
+        timer.after(1000, countdown)    
     
-    
-    
-    #
-    
-    timer['text'] = time_left
-    timer.after(1000, countdown)
 
 def lose():
     pass
 
 def guess(x: int, y: int):
-    #add to score depending on the guess, use current_photo_id as global
-    pass
+    """Update score given the guessed coordinates x and y.
+    
+    """
+    
+    global score
+    global current_photo_id
+    
+    score += calculate_score(x, y, current_photo_id)
+
+
+def next_round():
+    """Update current_photo_id to a random unused photo and reset time_left to 
+    30.
+    """
+    global current_photo_id
+    global unused_photos
+    global time_left
+    
+    current_photo_id = get_random_picture(unused_photos)
+    time_left = 30
 
 #start countdown
 timer.after(1000, countdown)
