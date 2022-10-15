80d48b4f52b3b58648f1d178aae0302f6664749d
Nitanon Rachapradit <98104447+Blue-Peanuts@users.noreply.github.com>
2022-10-15 10:27:38 -0400

    added countdown comments, lose() body, guess() body

--- frame_game.py
+++ frame_game.py
@@ -1,9 +1,12 @@
 import tkinter as tk
 from tkinter import *
 from tkinter import ttk
+import game_functions as gf
+
 
 time_left = 30
-
+current_photo_id = 0
+score = 0
 
 # create tkinter object, disable resize
 root = Tk()
@@ -23,8 +26,22 @@ timer.place(relx=0.5, rely=0.03, anchor='center')
 def countdown():
     global time_left
     time_left = time_left - 1
+    
+    #if the timer hits 0, call lose()
+    
+    
+    
+    #
+    
     timer['text'] = time_left
     timer.after(1000, countdown)
+
+def lose():
+    pass
+
+def guess(x: int, y: int):
+    #add to score depending on the guess, use current_photo_id as global
+    pass
 
 #start countdown
 timer.after(1000, countdown)
@@ -33,3 +50,6 @@ timer.after(1000, countdown)
 root.geometry("532x572")
 
 root.mainloop()
+
+
+# countdown()
