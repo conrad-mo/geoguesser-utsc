111c0367fa8139fb640fb8287368e146e55091d6
CherylZ4 <112735556+CherylZ4@users.noreply.github.com>
2022-10-15 10:23:01 -0400

    Implemented get_random_picture with docstring

--- game_functions.py
+++ game_functions.py
@@ -14,15 +14,18 @@ def calculate_score(x: int, y: int, photo_id: int) -> int:
 
 
 def get_photo_coords(photo_id: int) -> List[int]:
-    """Returns the coordinates of the photo given index photo_id
+    """Return the coordinates of the photo given index photo_id
         
     """
     return DB.coords [photo_id]
 
 
 def get_random_picture(lst: List[int]) -> int:
+    """Return a random coordinate from the list lst and then 
+       removes it from lst
+
     """
-    """
-    #choosing a random # from list 
-    # delete that number from the list
+    random_num = random.choice(lst)
+    lst.remove(random_num)
+  
     
