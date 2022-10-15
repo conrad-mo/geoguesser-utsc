04b1aa4491cbf729f9c49ffbc34512caf3791292
Shaul-Le <shaulwle@gmail.com>
2022-10-15 09:59:06 -0400

    Completed function calculate_score

--- game_functions.py
+++ game_functions.py
@@ -1,12 +1,17 @@
 from typing import List
+import math
 
 def calculate_score(x: int, y: int, photo_id: int) -> int:
+    """Return the score given the coordinates from x, y, and photo_id.
+    
     """
-    """
-    pass
+    distance = math.sqrt((x - get_photo_coords(photo_id)[0]) ** 2 + (y - 
+    get_photo_coords[1]) ** 2)
+    
+    return round(1000 / distance, 2)
 
 
-def get_photo_coords(photo_id: int) -> int:
+def get_photo_coords(photo_id: int) -> List[int]:
     """
     """
     pass
